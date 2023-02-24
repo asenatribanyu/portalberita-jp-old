79ae813ed18b554a8375d4ecff6aa5ca044c6934
zaldyalfarez <zaldyalfarez@gmail.com>
2023-02-24 13:32:16 +0700

    navbar sementara

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/home-design.css"> 
-    <title>Heiwa</title>
-</head>
-<body>
-    <h1>Halaman Home</h1>
-</body>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="stylesheet" href="css/home-design.css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap"
+            rel="stylesheet"
+        />
+        <title>Heiwa</title>
+    </head>
+    <body>
+        <nav class="navbar container">
+            <a href="/" class="logo">LOGO.</a>
+            <ul class="nav-list">
+                <li>
+                    <a href="/" class="nav-link">HOME</a>
+                </li>
+                <li>
+                    <a href="/" class="nav-link">ABOUT</a>
+                </li>
+                <li>
+                    <a href="/" class="nav-link">CATEGORIES</a>
+                </li>
+                <li>
+                    <a href="/" class="nav-link">LANGUAGE</a>
+                </li>
+                <li>
+                    <a href="/" class="nav-link">SEARCH</a>
+                </li>
+                <li>
+                    <a href="/" class="nav-link">SIGN IN</a>
+                </li>
+            </ul>
+
+            <div class="menu-toggle">
+                <input type="checkbox" />
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </nav>
+        <script src="js/home-script.js"></script>
+    </body>
 </html>

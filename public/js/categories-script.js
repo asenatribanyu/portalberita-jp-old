32a29953695df0ef5483581fe05bf750cc30afbc
asenatribanyu <asenatribanyu@gmail.com>
2023-02-28 10:53:41 +0700

    membenarkan dropdown

--- public/js/categories-script.js
+++ public/js/categories-script.js
@@ -1,3 +1,4 @@
+//dropdown
 const dropdowns = document.querySelectorAll(".dropdown");
 
 dropdowns.forEach((dropdown) => {
@@ -24,4 +25,30 @@ dropdowns.forEach((dropdown) => {
             option.classList.add("active");
         });
     });
+});
+
+//navbar
+const nav = document.querySelector("nav");
+const body = document.querySelector("body");
+const searchToggle = document.querySelector(".searchToggle");
+const sidebarC = document.querySelector(".side-close");
+const sidebarO = document.querySelector(".side-open");
+
+searchToggle.addEventListener("click", () => {
+    searchToggle.classList.toggle("active");
+});
+
+sidebarO.addEventListener("click", () => {
+    nav.classList.add("active");
+});
+
+body.addEventListener("click", (e) => {
+    let clicked = e.target;
+
+    if (
+        !clicked.classList.contains("side-open") &&
+        !clicked.classList.contains("menu")
+    ) {
+        nav.classList.remove("active");
+    }
 });
--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -11,11 +11,51 @@
             href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
         />
+        <link
+            href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+            rel="stylesheet"
+        />
 
         <title>Heiwa</title>
     </head>
     <body>
-        <h1>categories</h1>
+        <nav>
+            <div class="navbar">
+                <i class="bx bx-menu side-open"></i>
+                <span class="logo navLogo">
+                    <a href="/">Logo.</a>
+                </span>
+
+                <div class="menu-cat">
+                    <div class="logo-toggle">
+                        <span class="logo">
+                            <a href="/">Logo.</a>
+                        </span>
+                        <i class="bx bx-x side-close"></i>
+                    </div>
+
+                    <ul class="navlink">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/categories">Categories</a></li>
+                        <li><a href="/">Language</a></li>
+                        <li><a href="/login">Sign in</a></li>
+                    </ul>
+                </div>
+
+                <div class="searchBox">
+                    <div class="searchToggle">
+                        <i class="bx bx-x cancel"></i>
+                        <i class="bx bx-search search"></i>
+                    </div>
+
+                    <div class="searchField">
+                        <input type="text" placeholder="Search..." />
+                        <i class="bx bx-search search"></i>
+                    </div>
+                </div>
+            </div>
+        </nav>
         <div class="wrapper">
             <h3>sort by :</h3>
             <div class="dropdown">
@@ -36,10 +76,8 @@
                     <div class="caret"></div>
                 </div>
                 <ul class="menu">
-                    <li>Type 1</li>
-                    <li>Type 2</li>
-                    <li>Type 3</li>
-                    <li>Type 4</li>
+                    <li>Photo</li>
+                    <li>Video</li>
                 </ul>
             </div>
             <div class="dropdown">

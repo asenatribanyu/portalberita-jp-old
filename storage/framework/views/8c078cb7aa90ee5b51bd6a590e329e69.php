c852c6e7ef10dac814f83755aae4167a5e9e0c1f
asenatribanyu <asenatribanyu@gmail.com>
2023-03-16 12:57:34 +0700

    rollback

---
+++ storage/framework/views/8c078cb7aa90ee5b51bd6a590e329e69.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Heiwa | Articles</title>
+
+        <link rel="stylesheet" href="css/preview-design.css" />
+    </head>
+    <body>
+        <div class="row">
+            <div class="leftcolumn">
+                <div class="card">
+                    <div class="judul">
+                        <h2>Lorem Ipsum dolor sit amet</h2>
+                        <span class="tag tag-blue">Japan</span>
+                    </div>
+                    <small>by admin - DD/MM/YY</small>
+                    <div class="container1">
+                        <img src="img/2.jpg" alt="" width="840" height="420" />
+                    </div>
+                    <p>
+                        Sunt in culpa qui officia deserunt mollit anim id est
+                        laborum consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut
+                        enim ad minim veniam, quis nostrud exercitation ullamco.
+                        Sunt in culpa qui officia deserunt mollit anim id est
+                        laborum consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut
+                        enim ad minim veniam, quis nostrud exercitation ullamco.
+                        Sunt in culpa qui officia deserunt mollit anim id est
+                        laborum consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut
+                        enim ad minim veniam, quis nostrud exercitation ullamco.
+                        Sunt in culpa qui officia deserunt mollit anim id est
+                        laborum consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut
+                        enim ad minim veniam, quis nostrud exercitation ullamco.
+                    </p>
+                </div>
+                <div class="card">
+                    <h2>TITLE HEADING</h2>
+                    <h5>Title description, Sep 2, 2017</h5>
+                    <div class="fakeimg" style="height: 200px">Image</div>
+                    <p>Some text..</p>
+                    <p>
+                        Sunt in culpa qui officia deserunt mollit anim id est
+                        laborum consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut
+                        enim ad minim veniam, quis nostrud exercitation ullamco.
+                    </p>
+                </div>
+            </div>
+            <div class="rightcolumn">
+                <div class="card">
+                    <h2>About Me</h2>
+                    <div class="fakeimg" style="height: 100px">Image</div>
+                    <p>
+                        Some text about me in culpa qui officia deserunt mollit
+                        anim..
+                    </p>
+                </div>
+                <div class="card">
+                    <h3>Popular Post</h3>
+                    <div class="fakeimg"><p>Image</p></div>
+                    <div class="fakeimg"><p>Image</p></div>
+                    <div class="fakeimg"><p>Image</p></div>
+                </div>
+                <div class="card">
+                    <h3>Follow Me</h3>
+                    <p>Some text..</p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+<?php /**PATH C:\Kuliah\mbkm\portalberita-jp\resources\views/preview.blade.php ENDPATH**/ ?>
